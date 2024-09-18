352c98e8870db48d90291ed7e40e012ffd1831ff
Florencio03 <florencio.pool@hotmail.com>
2024-09-17 18:49:04 -0600

    add CD_01

---
+++ CD_01_Presentacion_2023/ADA 1/idlePython_Florencio_AnguloPool.py
@@ -0,0 +1,15 @@
+# Cargar las biblioteca Pandas con el alias 'pd’
+# Cargar las biblioteca Matplotlib con el alias ‘plt’
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Leer datos del archivo datos.csv
+data = pd.read_csv ("datos/Gasoline_Prices.csv")
+
+# Vista previa de las primeras 5 líneas de los datos cargados
+print(data.head ())
+
+
+#Graficamos las variables
+data.plot(x ='Year', y='Gasoline Prices', kind = 'line', figsize=(25,6))
+plt.show()
